c69f8aea8aa6347a00533fe8f3346d038bf2fba1
Xuefeng, Ding <xuefeng.ding.physics@gmail.com>
2022-03-25 21:07:35 -0400

    add main function; add jar configuration.

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.example.helloworld;
+/**
+ * Created: 3/25/22, Friday
+ *
+ * @author <a href="mailto:xuefeng.ding.physics@gmail.com">Xuefeng Ding</a>
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
